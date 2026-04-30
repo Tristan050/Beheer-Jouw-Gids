c90c65ad13077cf19a0c3abe71511d4460431b87
Colin Falkenburg <colinfalkenburg@gmail.com>
2026-04-30 12:50:53 +0200

    staging bugfixes: timestamp naar unix in OTPrepo, dotenv fix + mail fix

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 
-$db_host = getenv("DB_HOST");
-$db_dbs = getenv("DB_NAME");
-$db_user = getenv("DB_USER");
-$db_pass = getenv("DB_PASS");
+$db_host = $_ENV['DB_HOST'] ?? getenv('DB_HOST');
+$db_dbs = $_ENV['DB_NAME'] ?? getenv('DB_NAME');
+$db_user = $_ENV['DB_USER'] ?? getenv('DB_USER');
+$db_pass = $_ENV['DB_PASS'] ?? getenv('DB_PASS');
 
 $appBaseUrl = rtrim((string) getenv('APP_BASE_URL'), '/');
 $googleClientId = (string) getenv('GOOGLE_CLIENT_ID');
